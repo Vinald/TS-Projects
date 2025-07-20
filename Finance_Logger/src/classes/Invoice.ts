3488a22945941ba863a5fae532c0b9c62017fc6c
[vinald] <okiror1vinald@gmail.com>
2025-07-20 10:50:22 +0300

    Linking the different components

--- Finance_Logger/src/classes/Invoice.ts
+++ Finance_Logger/src/classes/Invoice.ts
@@ -1,11 +1,7 @@
 import { HasFormatter } from "../interfaces/HasFormatter";
 
 export class Invoice implements HasFormatter {
-    constructor(
-        private client: string,
-        private details: string,
-        private amount: number) {
-    }
+    constructor(private client: string, private details: string, private amount: number) {}
 
     format() {
         return `${this.client} owes ${this.amount} for ${this.details}`;
--- Finance_Logger/src/classes/Payments.ts
+++ Finance_Logger/src/classes/Payments.ts
@@ -1,11 +1,7 @@
 import { HasFormatter } from "../interfaces/HasFormatter";
 
 export class Payments implements HasFormatter {
-    constructor(
-        private recipient: string,
-        private details: string,
-        private amount: number) {
-    }
+    constructor(private recipient: string, private details: string, private amount: number) {}
 
     format() {
         return `${this.recipient} is owed ${this.amount} for ${this.details}`;
